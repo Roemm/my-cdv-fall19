93ab5528d7e72ae70c0a54534787426d7885e3b2
Yiqi Chang <yc2548@nyu.edu>
2019-09-11 10:37:35 +0800

    updates servey data

--- my-work/week1/lab1/js/script.js
+++ my-work/week1/lab1/js/script.js
@@ -1,182 +1,174 @@
 let data = [
     {
-        "timestamp": "2019-09-03T14:44:28.271Z",
-        "milkTea": 3,
-        "ramen": 7,
-        "spaghettiBolognese": 5,
-        "mango": 2
+        "time": "2019-09-09T07:40:40.809Z",
+        "bts": 3,
+        "exo": 8,
+        "got7": 9,
+        "superJunior": 10,
+        "bigbang": 6,
+        "girlsGeneration": 10,
+        "2ne1": 9,
+        "twice": 9,
+        "blackpink": 5,
+        "redVelvet": 7
     },
     {
-        "timestamp": "2019-09-03T14:44:36.101Z",
-        "milkTea": 2,
-        "ramen": 2,
-        "spaghettiBolognese": 8,
-        "mango": 4
+        "time": "2019-09-09T07:42:26.711Z",
+        "bts": 6,
+        "exo": 5,
+        "got7": 8,
+        "superJunior": 5,
+        "bigbang": 6,
+        "girlsGeneration": 7,
+        "2ne1": 7,
+        "twice": 8,
+        "blackpink": 7,
+        "redVelvet": 7
     },
     {
-        "timestamp": "2019-09-03T14:45:10.480Z",
-        "milkTea": 2,
-        "ramen": 3,
-        "spaghettiBolognese": 4,
-        "mango": 1
+        "time": "2019-09-09T07:44:11.739Z",
+        "bts": 4,
+        "exo": 7,
+        "got7": 8,
+        "superJunior": 9,
+        "bigbang": 8,
+        "girlsGeneration": 6,
+        "2ne1": 6,
+        "twice": 5,
+        "blackpink": 5,
+        "redVelvet": 7
     },
     {
-        "timestamp": "2019-09-04T05:48:07.519Z",
-        "milkTea": 10,
-        "ramen": 10,
-        "spaghettiBolognese": 7,
-        "mango": 10,
-        "dumpling": 8
+        "time": "2019-09-09T07:48:01.202Z",
+        "bts": 5,
+        "exo": 7,
+        "got7": 10,
+        "superJunior": 7,
+        "bigbang": 7,
+        "girlsGeneration": 8,
+        "2ne1": 5,
+        "twice": 9,
+        "blackpink": 8,
+        "redVelvet": 8
     },
     {
-        "timestamp": "2019-09-04T05:48:11.427Z",
-        "milkTea": 7,
-        "ramen": 5,
-        "spaghettiBolognese": 6,
-        "mango": 1,
-        "dumpling": 7
+        "time": "2019-09-09T07:56:38.829Z",
+        "bts": 5,
+        "exo": 1,
+        "got7": 10,
+        "superJunior": 6,
+        "bigbang": 4,
+        "girlsGeneration": 3,
+        "2ne1": 4,
+        "twice": 8,
+        "blackpink": 3,
+        "redVelvet": 5
     },
     {
-        "timestamp": "2019-09-04T05:48:11.509Z",
-        "milkTea": 7,
-        "ramen": 9,
-        "spaghettiBolognese": 10,
-        "mango": 1,
-        "dumpling": 8
+        "time": "2019-09-09T08:23:04.372Z",
+        "bts": 5,
+        "exo": 4,
+        "got7": 5,
+        "superJunior": 5,
+        "bigbang": 6,
+        "girlsGeneration": 5,
+        "2ne1": 5,
+        "twice": 5,
+        "blackpink": 7,
+        "redVelvet": 5
     },
     {
-        "timestamp": "2019-09-04T05:48:15.178Z",
-        "milkTea": 10,
-        "ramen": 10,
-        "spaghettiBolognese": 4,
-        "mango": 10,
-        "dumpling": 10
+        "time": "2019-09-09T08:29:39.160Z",
+        "bts": 5,
+        "exo": 10,
+        "got7": 10,
+        "superJunior": 5,
+        "bigbang": 5,
+        "girlsGeneration": 9,
+        "2ne1": 5,
+        "twice": 9,
+        "blackpink": 9,
+        "redVelvet": 6
     },
     {
-        "timestamp": "2019-09-04T05:48:19.690Z",
-        "milkTea": 5,
-        "ramen": 10,
-        "spaghettiBolognese": 8,
-        "mango": 8,
-        "dumpling": 7
+        "time": "2019-09-09T10:49:07.517Z",
+        "bts": 7,
+        "exo": 5,
+        "got7": 5,
+        "superJunior": 5,
+        "bigbang": 5,
+        "girlsGeneration": 5,
+        "2ne1": 5,
+        "twice": 3,
+        "blackpink": 9,
+        "redVelvet": 6
     },
     {
-        "timestamp": "2019-09-04T05:48:23.595Z",
-        "milkTea": 1,
-        "ramen": 10,
-        "spaghettiBolognese": 10,
-        "mango": 10,
-        "dumpling": 5
+        "time": "2019-09-09T13:22:19.831Z",
+        "bts": 9,
+        "exo": 1,
+        "got7": 5,
+        "superJunior": 6,
+        "bigbang": 6,
+        "girlsGeneration": 8,
+        "2ne1": 3,
+        "twice": 9,
+        "blackpink": 7,
+        "redVelvet": 8
     },
     {
-        "timestamp": "2019-09-04T05:48:32.244Z",
-        "milkTea": 10,
-        "ramen": 10,
-        "spaghettiBolognese": 9,
-        "mango": 1,
-        "dumpling": 8
-    },
-    {
-        "timestamp": "2019-09-04T05:48:35.279Z",
-        "milkTea": 10,
-        "ramen": 9,
-        "spaghettiBolognese": 8,
-        "mango": 10,
-        "dumpling": 7
-    },
-    {
-        "timestamp": "2019-09-04T05:48:36.009Z",
-        "milkTea": 10,
-        "ramen": 10,
-        "spaghettiBolognese": 9,
-        "mango": 10,
-        "dumpling": 9
-    },
-    {
-        "timestamp": "2019-09-04T05:48:40.293Z",
-        "milkTea": 10,
-        "ramen": 10,
-        "spaghettiBolognese": 10,
-        "mango": 10,
-        "dumpling": 10
-    },
-    {
-        "timestamp": "2019-09-04T05:48:40.671Z",
-        "milkTea": 8,
-        "ramen": 8,
-        "spaghettiBolognese": 7,
-        "mango": 9,
-        "dumpling": 7
-    },
-    {
-        "timestamp": "2019-09-04T05:49:14.027Z",
-        "milkTea": 1,
-        "ramen": 10,
-        "spaghettiBolognese": 1,
-        "mango": 10,
-        "dumpling": 1
+        "time": "2019-09-11T01:54:02.122Z",
+        "bts": 5,
+        "exo": 7,
+        "got7": 9,
+        "superJunior": 6,
+        "bigbang": 6,
+        "girlsGeneration": 10,
+        "2ne1": 5,
+        "twice": 9,
+        "blackpink": 8,
+        "redVelvet": 9
     }
 ]
 
-// the function dates a data
-// arrayn as an argument
+
 function averageData(data){
-  // new empty array to be filled
-  // with data in new structure
+
   let newData = [];
-  // assuming each data point has the same
-  // keys/categories, we extract an array of them from the
-  // first data point in the array
+
   let keys = Object.keys(data[data.length-1]);
-  // console.log(keys);
-  // now we loop over the keys/categories
+
   for(let i = 0; i < keys.length; i++){
-    // store the current key/category in
-    // a variable:
     let key = keys[i];
-    // now we will loop over each data point
-    // in the data set, check if it has a value
-    // for the key/category and add them to
-    // a total sum variable
-    // as well as count the occurences in order to
-    // calulate the averae in the end
     let sum = 0;
     let num = 0;
     for(let j = 0; j < data.length; j++){
       let datum = data[j];
-      // check if the key exists
-      // for this datapoint
+
       if(key in datum){
-        // add to sum
         sum += datum[key];
-        // increase count
         num++;
       }
     }
-    // now calculate the average
+
     let avg = sum/num;
-    // make sure the value is a number
-    // (some value might be strings)
+
     if(!isNaN(avg)){
-      // create an object with both the average
-      // and also the number of measurements that
-      // went into the average
+
       let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
-      // add the new datapoint to the new data array
+
       newData.push(newDataPoint);
     }
   }
-  // return everything when it is done
+
   return newData;
 }
 
 
 let transformedData = averageData(data);
-console.log(transformedData)
 
-//loop from transformedData
 for(let i = 0; i<transformedData.length; i++){
-  //for each item get the name and the average value
+
   let datapoint = transformedData[i];
   let food = datapoint.name;
   let average = datapoint.average;
@@ -184,10 +176,10 @@ for(let i = 0; i<transformedData.length; i++){
   console.log(food);
   console.log(average);
 
-  //create a div
+
   let bar = document.createElement('div');
   let barname = document.createElement('p');
-  barname.innerHTML = datapoint.name;
+  barname.innerHTML = datapoint.name + ": " + datapoint.average;
 
   //modify bar size
   bar.className = "bar";
@@ -196,6 +188,5 @@ for(let i = 0; i<transformedData.length; i++){
   bar.appendChild(barname);
   document.body.appendChild(bar);
 
-  //modify the size,
 
 }
