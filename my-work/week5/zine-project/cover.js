e768c8d2ff74eaec8efb98c873c4ff837f4e310f
Yiqi Chang <yc2548@nyu.edu>
2019-10-13 01:53:31 +0800

    update zine project

---
+++ my-work/week5/zine-project/cover.js
@@ -0,0 +1,104 @@
+let y = 0;
+let col = 8;
+let w = 1200;
+let h = 800;
+let margin = { top: 0, right: w/6, bottom: h/4, left: w/6 };
+
+let border=12;
+let bordercolor='#E8E1D3';
+let backcolor = '#9E9480';
+let containerColor = '#DFD1B5';
+
+//create svg
+let viz = d3.select('#container')
+  .style('background-color', backcolor)
+  .append('svg')
+    .attr('width', w*2/3)
+    .attr('height', h*3.5/6)
+    .attr('id', 'viz')
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .style("background-color", containerColor)
+  ;
+
+//set the position of each element
+//so that it is positioned in a grid
+function setX(datapoint,i){
+  return 100 * (i % col)  + 55;
+}
+
+function setX2(datapoint,i){
+  // console.log(datapoint.time *10);
+  return 100 * (i % col) + 65 + datapoint.time*1.2;
+}
+
+
+function setY(datapoint,i){
+  if(i == 0){
+    y = 0;
+  }
+  if (i % col == 0){
+    y++;
+  }
+  return 75 * y - 30;
+}
+
+
+//decide the color of the shape based on what day it is
+function getColor(datapoint){
+  if (datapoint.date == 'Wed') {
+    return '#CCC6BA';
+  }else if (datapoint.date == 'Thu') {
+    return '#3D5578';
+  }else if(datapoint.date == 'Fri'){
+    return '#A6A197';
+  }else if(datapoint.date == 'Sat'){
+    return '#5F779C';
+  }else if(datapoint.date == 'Sun'){
+    return '#918367';
+  }else if(datapoint.date == 'Mon'){
+    return '#9FAFC4';
+  }
+}
+
+
+function gotData(incomingData){
+  console.log(incomingData);
+
+  //create groupelements
+  let groupelements = viz.selectAll(".datagroup").data(incomingData)
+  .enter()
+    .append("g")
+    .attr("class", "datagroup")
+  ;
+
+  //append the first circle
+  groupelements.append("circle")
+    .attr("cx", setX)
+    .attr("cy", setY)
+    .attr("r", 30)
+    .attr('fill', getColor)
+    // .attr('fill-opacity', getOpa)
+  ;
+
+  //append the second circle to create the moon shape
+  groupelements.append("circle")
+    .attr("cx", setX2)
+    .attr("cy", setY)
+    .attr("r", 25)
+    .attr('fill', containerColor)
+    // .attr('stroke', 'black')
+    // .attr('stroke-width', 2)
+  ;
+
+  var borderPath = viz.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("height", h*3.5/6)
+    .attr("width", w*2/3)
+    .style("stroke", bordercolor)
+    .style("fill", "none")
+    .style("stroke-width", border);
+}
+
+
+d3.json("data.json").then(gotData);
---
+++ my-work/week5/zine-project/middlescript.js
@@ -0,0 +1,186 @@
+let w = 2400;
+let h = 800;
+
+let containerColor = '#DFD1B5';
+
+let viz = d3.select('#container')
+  .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .style('background-color', containerColor)
+    .attr('class', 'chart')
+;
+
+
+
+function getColor(datapoint){
+  let idx = datapoint.index;
+  let day = datapoint[0].data.values[idx].date;
+  console.log(datapoint);
+  if (day == 'Wed') {
+    return 'CCC6BA';
+  }else if (day == 'Thu') {
+    return '#3D5578';
+  }else if(day == 'Fri'){
+    return '#A6A197';
+  }else if(day == 'Sat'){
+    return '#5F779C';
+  }else if(day == 'Sun'){
+    return '#918367';
+  }else if(day == 'Mon'){
+    return '#9FAFC4';
+  }
+}
+
+
+function gotData(incomingData){
+
+  //console.log(incomingData);
+  let yearToDataConverter = d3.timeParse('%H-%H');
+
+  let alterDomainArray = d3.extent(incomingData, function(d){
+    return yearToDataConverter(d.timeperiod);
+  });
+
+   // console.log(alterDomainArray);
+
+  //create x and y axis
+  let xPadding = 100;
+  let xScale = d3.scaleTime().domain(alterDomainArray).range([xPadding, w-(xPadding*1.5)]);
+  // console.log(xScale('9-11'));
+
+  let xAxis = d3.axisBottom(xScale);
+  xAxis.tickArguments([d3.timeHour.every(2)]);
+  let xAxisGroup = viz.append('g').attr('class', 'xaxis');
+
+  xAxisGroup.call(xAxis);
+
+  let xAxisYPos = h - 50;
+  xAxisGroup.attr('transform', 'translate(0, '+xAxisYPos + ')')
+    .style('font-family', "'Ruda', sans-serif")
+    .style('font-size', 20)
+    .style('color', '#5E584C')
+    .style('stroke-width', 3)
+  ;
+
+
+  //one array for each value of x-axis
+  // console.log(incomingData);
+  let sumstat = d3.nest()
+    .key(function(d) { return d.timeperiod;})
+    .entries(incomingData);
+  console.log(sumstat);
+
+  var mygroup = [0,1,2,3,4,5];
+
+  var stackTheData = d3.stack()
+    .keys(mygroup)
+    .value(function(d, i){
+      console.log(d, i);
+      // for(j=0; j<d.values.length;j++){
+        return d.values[i].time;
+      // }
+    });
+  let stackedData = stackTheData(sumstat);
+
+  console.log(stackedData);
+
+  let yScale = d3.scaleLinear()
+    .domain([0,d3.max(stackedData[stackedData.length-1], function(d){
+      return d[1];
+    })]).range([xAxisYPos , 50]);
+
+  // let y = d3.scaleLinear()
+  //     .domain([0, d3.max(incomingData, function(d) { return +d.time; })*1.2])
+  //     .range([ h, 0 ]);
+
+  let yAxis = d3.axisLeft(yScale);
+  let yAxisGroup = viz.append('g').attr('class', 'yaxis');
+
+  yAxisGroup.call(yAxis);
+  yAxisGroup.attr('transform', 'translate(' + xPadding + ', 0)')
+    .style('font-family', "'Ruda', sans-serif")
+    .style('font-size', 20)
+    .style('color', '#5E584C')
+    .style('stroke-width', 3)
+  ;
+
+  let vizGroup = viz.append("g").attr("class", "vizgroup");
+
+  let curve = d3.area()
+    .x(function(d, i) {
+      console.log( xScale(yearToDataConverter(d.data.key)) );
+      return xScale(yearToDataConverter(d.data.key)) + 2 ;
+    })
+    .y0(function(d) { return yScale(d[0]) -1; })
+    .y1(function(d) { return yScale(d[1]) -1; })
+
+  vizGroup
+    .selectAll("path")
+    .data(stackedData)
+    .enter()
+    .append("path")
+      .style("fill", getColor)
+      .attr("d", curve)
+  ;
+
+  let note = d3.select(".chart").append("g")
+      .attr("class","note")
+  ;
+  let notation = note
+    .selectAll('note')
+    .data(stackedData)
+    .enter()
+    .append('g', 'notation');
+    // .attr('class', 'notation');
+
+  notation.append('text')
+      .text(function(d){
+        let idx = d.index;
+        return d[0].data.values[idx].date;
+      })
+      .style('fill', '#5E584C')
+      .attr("x",2320)
+      .attr("y",function(d, i){return 145 + i*50;})
+      .style("text-anchor","end")
+      .style('font-size', 25)
+      .style('font-family', "'Ruda', sans-serif")
+  ;
+
+  notation.append('rect')
+    .attr("x",2330)
+    .attr("y",function(d, i){return 120 + i*50;})
+    .attr('width', 40)
+    .attr('height', 30)
+    .style('fill', getColor)
+  ;
+
+
+}
+
+d3.json("data.json").then(gotData);
+
+
+
+
+// // bind data and create groups for each datapoint:
+// let dataGroups = vizGroup.selectAll(".datagroup").data(incomingData).enter()
+//     .append("g")
+//     .attr("class", "datagroup")
+// ;
+//
+//
+// let circles = dataGroups.append("circle")
+//     .attr("cx", 0)
+//     .attr("cy", 0)
+//     .attr("r", 5)
+//     .attr('fill', getColor)
+// ;
+//
+// function getTranslate(d, i){
+//   let properlyFormattedDate = yearToDataConverter(d.timeperiod);
+//   let value = d.time;
+//   return "translate("+xScale(properlyFormattedDate)+","+yScale(value)+")";
+// }
+// // translate the position of each group:
+// dataGroups.attr("transform", getTranslate);
